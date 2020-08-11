bb7afca4f5fe632679fdebd527120eff036f79e2
Amandeep-Bhullar <er.amandeepbhullar@gmail.com>
2020-08-10 21:03:16 -0400

    changed to heroku url for socket connection

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,8 @@ import { USER_CONNECTED, LOGOUT } from '../Events'
 import LoginForm from './LoginForm'
 import ChatContainer from './chats/ChatContainer'
 
-const socketUrl = "http://localhost:3231"
+// const socketUrl = "http://localhost:3231"
+const socketUrl = "https://aurnab-message.herokuapp.com/"
 export default class Layout extends Component {
 	
 	constructor(props) {
